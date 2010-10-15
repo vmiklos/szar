bcff9db9e5130bfe8b8b5823467a28d8c55ad989
Miklos Vajna <vmiklos@frugalware.org>
2010-10-16 01:38:46 +0200

    less omniorb-specific message when it's just generic corba

--- examples/corba/client.cc
+++ examples/corba/client.cc
@@ -8,11 +8,9 @@ static CORBA::Object_ptr getObjectReference(CORBA::ORB_ptr orb, const char *id)
 	CosNaming::NamingContext_var rootContext;
 
 	try {
-		// Obtain a reference to the root context of the Name service:
 		CORBA::Object_var obj;
 		obj = orb->resolve_initial_references("NameService");
 
-		// Narrow the reference returned.
 		rootContext = CosNaming::NamingContext::_narrow(obj);
 		if( CORBA::is_nil(rootContext) ) {
 			cerr << "Failed to narrow the root naming context." << endl;
@@ -20,42 +18,34 @@ static CORBA::Object_ptr getObjectReference(CORBA::ORB_ptr orb, const char *id)
 		}
 	}
 	catch (CORBA::NO_RESOURCES&) {
-		cerr << "Caught NO_RESOURCES exception. You must configure omniORB "
-			<< "with the location" << endl
+		cerr << "Caught NO_RESOURCES exception. You must configure "
+			<< "the location" << endl
 			<< "of the naming service." << endl;
 		return 0;
 	}
 	catch(CORBA::ORB::InvalidName& ex) {
-		// This should not happen!
 		cerr << "Service required is invalid [does not exist]." << endl;
 		return CORBA::Object::_nil();
 	}
 
-	// Create a name object, containing the name test/context:
 	CosNaming::Name name;
 	name.length(2);
 
-	name[0].id   = (const char*) "test";       // string copied
-	name[0].kind = (const char*) "my_context"; // string copied
+	name[0].id   = (const char*) "szoftarch";
+	name[0].kind = (const char*) "scm";
 	name[1].id   = (const char*) id;
 	name[1].kind = (const char*) "Object";
-	// Note on kind: The kind field is used to indicate the type
-	// of the object. This is to avoid conventions such as that used
-	// by files (name.type -- e.g. test.ps = postscript etc.)
 
 	try {
-		// Resolve the name to an object reference.
 		return rootContext->resolve(name);
 	}
 	catch(CosNaming::NamingContext::NotFound& ex) {
-		// This exception is thrown if any of the components of the
-		// path [contexts or the object] aren't found:
 		cerr << "Context not found." << endl;
 	}
 	catch(CORBA::TRANSIENT& ex) {
 		cerr << "Caught system exception TRANSIENT -- unable to contact the "
 			<< "naming service." << endl
-			<< "Make sure the naming server is running and that omniORB is "
+			<< "Make sure the naming server is running and "
 			<< "configured correctly." << endl;
 
 	}
@@ -111,4 +101,3 @@ int main (int argc, char **argv)
 	}
 	return 0;
 }
-
--- examples/corba/server.cc
+++ examples/corba/server.cc
@@ -22,11 +22,9 @@ static CORBA::Boolean bindObjectToName(CORBA::ORB_ptr orb, CORBA::Object_ptr obj
 	CosNaming::NamingContext_var rootContext;
 
 	try {
-		// Obtain a reference to the root context of the Name service:
 		CORBA::Object_var obj;
 		obj = orb->resolve_initial_references("NameService");
 
-		// Narrow the reference returned.
 		rootContext = CosNaming::NamingContext::_narrow(obj);
 		if(CORBA::is_nil(rootContext)) {
 			cerr << "Failed to narrow the root naming context." << endl;
@@ -34,37 +32,27 @@ static CORBA::Boolean bindObjectToName(CORBA::ORB_ptr orb, CORBA::Object_ptr obj
 		}
 	}
 	catch (CORBA::NO_RESOURCES&) {
-		cerr << "Caught NO_RESOURCES exception. You must configure omniORB "
-			<< "with the location" << endl
+		cerr << "Caught NO_RESOURCES exception. You must configure "
+			<< "the location" << endl
 			<< "of the naming service." << endl;
 		return 0;
 	}
 	catch (CORBA::ORB::InvalidName&) {
-		// This should not happen!
 		cerr << "Service required is invalid [does not exist]." << endl;
 		return 0;
 	}
 
 	try {
-		// Bind a context called "test" to the root context:
-
 		CosNaming::Name contextName;
 		contextName.length(1);
-		contextName[0].id   = (const char*) "test";       // string copied
-		contextName[0].kind = (const char*) "my_context"; // string copied
-		// Note on kind: The kind field is used to indicate the type
-		// of the object. This is to avoid conventions such as that used
-		// by files (name.type -- e.g. test.ps = postscript etc.)
+		contextName[0].id   = (const char*) "szoftarch";
+		contextName[0].kind = (const char*) "scm";
 
 		CosNaming::NamingContext_var testContext;
 		try {
-			// Bind the context to root.
 			testContext = rootContext->bind_new_context(contextName);
 		}
 		catch(CosNaming::NamingContext::AlreadyBound& ex) {
-			// If the context already exists, this exception will be raised.
-			// In this case, just resolve the name and assign testContext
-			// to the object returned:
 			CORBA::Object_var obj;
 			obj = rootContext->resolve(contextName);
 			testContext = CosNaming::NamingContext::_narrow(obj);
@@ -74,33 +62,26 @@ static CORBA::Boolean bindObjectToName(CORBA::ORB_ptr orb, CORBA::Object_ptr obj
 			}
 		}
 
-		// Bind objref with name Echo to the testContext:
 		CosNaming::Name objectName;
 		objectName.length(1);
-		objectName[0].id   = (const char*) name;   // string copied
-		objectName[0].kind = (const char*) "Object"; // string copied
+		objectName[0].id   = (const char*) name;
+		objectName[0].kind = (const char*) "Object";
 
+		// It is necessary to first try bind and then rebind, as rebind
+		// on it's own will throw a NotFoundexception if the Name has
+		// not already been bound. [This is incorrect behaviour - it
+		// should just bind].
 		try {
 			testContext->bind(objectName, objref);
 		}
 		catch(CosNaming::NamingContext::AlreadyBound& ex) {
 			testContext->rebind(objectName, objref);
 		}
-		// Note: Using rebind() will overwrite any Object previously bound
-		//       to /test/Echo with obj.
-		//       Alternatively, bind() can be used, which will raise a
-		//       CosNaming::NamingContext::AlreadyBound exception if the name
-		//       supplied is already bound to an object.
-
-		// Amendment: When using OrbixNames, it is necessary to first try bind
-		// and then rebind, as rebind on it's own will throw a NotFoundexception if
-		// the Name has not already been bound. [This is incorrect behaviour -
-		// it should just bind].
 	}
 	catch(CORBA::TRANSIENT& ex) {
 		cerr << "Caught system exception TRANSIENT -- unable to contact the "
 			<< "naming service." << endl
-			<< "Make sure the naming server is running and that omniORB is "
+			<< "Make sure the naming server is running and "
 			<< "configured correctly." << endl;
 
 		return 0;
@@ -133,6 +114,8 @@ int main(int argc, char **argv)
 
 		// Enter main loop
 		orb->run();
+	} catch(CORBA::TRANSIENT& ex) {
+		cerr << "fatal: can't connect to the name server" << endl;
 	} catch(CORBA::SystemException& ex) {
 		cerr << "Caught CORBA::" << ex._name() << endl;
 	} catch(CORBA::Exception& ex) {
