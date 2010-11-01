33b29f6ea9bdec520f7f1dc1a5b9c48c00af434d
Miklos Vajna <vmiklos@frugalware.org>
2010-11-01 01:59:38 +0100

    AdminImpl::addModel implementation

--- server/AdminImpl.cc
+++ server/AdminImpl.cc
@@ -6,10 +6,29 @@ void AdminImpl::setUid(int value) {
 
 VersionControl::Model_ptr AdminImpl::addModel(const char* name)
 {
-	cout << "TODO: AdminImpl::addModel()" << endl;
+	QSqlDatabase db = QSqlDatabase::database();
+	QSqlQuery q(db);
+	q.prepare("select id from models where name = :name");
+	q.bindValue(":name", name);
+	if (q.exec()) {
+		if (q.size() > 0 && q.next()) {
+			VersionControl::Model_ptr model = RootImpl::modelFromId(q, uid);
+			throw VersionControl::AlreadyExistsException(model);
+		} else {
+			q.prepare("insert into models (name) values (:name)");
+			q.bindValue(":name", name);
+			if (!q.exec()) throw VersionControl::DbError();
+			q.prepare("select id from models where name = :name");
+			q.bindValue(":name", name);
+			if (q.exec() && q.next())
+				return RootImpl::modelFromId(q, uid);
+		}
+	} else {
+		cerr << "AdminImpl::addModel() Error occured during SQL query: " << q.lastError().text().toStdString() << endl;
+	}
 }
 
-VersionControl::UserAdmin_ptr AdminImpl::addUser(const char* name)
+VersionControl::UserAdmin_ptr AdminImpl::addUser(const char* /*name*/)
 {
 	cout << "TODO: AdminImpl::addUser()" << endl;
 }
@@ -19,12 +38,12 @@ VersionControl::UserAdminSeq* AdminImpl::getUsers()
 	cout << "TODO: VersionControl::UserAdminSeq()" << endl;
 }
 
-void AdminImpl::removeUser(VersionControl::UserAdmin_ptr user)
+void AdminImpl::removeUser(VersionControl::UserAdmin_ptr /*user*/)
 {
 	cout << "TODO: AdminImpl::removeUser()" << endl;
 }
 
-VersionControl::ModelAdmin_ptr AdminImpl::getModelAdmin(VersionControl::Model_ptr target)
+VersionControl::ModelAdmin_ptr AdminImpl::getModelAdmin(VersionControl::Model_ptr /*target*/)
 {
 	cout << "TODO: AdminImpl::getModelAdmin()" << endl;
 }
