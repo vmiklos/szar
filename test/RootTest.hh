50ebda0c3828f5608434a564ce5b8fd37058aa64
Miklos Vajna <vmiklos@frugalware.org>
2010-11-06 01:56:10 +0100

    Root: test getModel, getMyUser and getResolver

--- test/RootTest.hh
+++ test/RootTest.hh
@@ -1,10 +1,14 @@
-/// Tests the Root interface. Status: tests only getAdmin()
+/// Tests the Root interface. Status: everything tested.
 class RootTest : public CppUnit::TestFixture
 {
 	CPPUNIT_TEST_SUITE(RootTest);
 	CPPUNIT_TEST(testGetAdminSuccess);
 	CPPUNIT_TEST(testGetAdminFailure);
 	CPPUNIT_TEST(testGetModels);
+	CPPUNIT_TEST(testGetModelSuccess);
+	CPPUNIT_TEST(testGetModelFailure);
+	CPPUNIT_TEST(testGetMyUser);
+	CPPUNIT_TEST(testGetResolver);
 	CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -67,6 +71,36 @@ public:
 	void testGetModels()
 	{
 		VersionControl::Root_var root = authref->login("admin", "admin");
-		CPPUNIT_ASSERT(root->getModels()->length() == 0);
+		CPPUNIT_ASSERT(root->getModels()->length() == 1);
+	}
+
+	void testGetModelSuccess()
+	{
+		VersionControl::Root_var root = authref->login("admin", "admin");
+		root->getModel("foo");
+	}
+
+	void testGetModelFailure()
+	{
+		VersionControl::Root_var root = authref->login("admin", "admin");
+		int failed = 0;
+		try {
+			root->getModel("bar");
+		} catch (VersionControl::InvalidModel& e) {
+			failed = 1;
+		}
+		CPPUNIT_ASSERT(failed == 1);
+	}
+
+	void testGetMyUser()
+	{
+		VersionControl::Root_var root = authref->login("admin", "admin");
+		root->getMyUser();
+	}
+
+	void testGetResolver()
+	{
+		VersionControl::Root_var root = authref->login("admin", "admin");
+		root->getResolver();
 	}
 };
--- test/RootTest.sql
+++ test/RootTest.sql
@@ -1,3 +1,7 @@
 truncate table users;
 insert into users (username, password, admin) values ("admin", SHA1("admin"), 1);
 insert into users (username, password, admin) values ("john", SHA1("foo"), 0);
+truncate table models;
+insert into models (name) values ("foo");
+truncate table acl;
+insert into acl (user_id, model_id, rights) values (1, 1, "ReadWrite");
