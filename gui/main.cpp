495f8e48dc242a4d2ad7a4c51288a74d75898d80
András Veres-Szentkirályi <vsza@vsza.hu>
2010-11-09 17:47:12 +0100

    Added CORBA connection and model enumeration to GUI

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,25 +1,70 @@
 #include "ui_main.h"
 #include "ui_connect.h"
+#include <VersionControl.hh>
+#include <QMessageBox>
 
-QWidget *showMainWindow() {
-	QMainWindow *mw = new QMainWindow;
-	Ui::MainWindow ui;
-	ui.setupUi(mw);
-	mw->show();
-	return mw;
-}
-
-void showConnectDialog(QWidget *mw) {
+VersionControl::Root_ptr showConnectDialog(QWidget *mw, CORBA::ORB_var orb) {
 	QDialog *cd = new QDialog(mw, Qt::WindowSystemMenuHint | Qt::WindowTitleHint);
 	Ui::ConnectDialog ui;
 	ui.setupUi(cd);
-	if (cd->exec() != QDialog::Accepted) exit(0);
+	while (true) {
+		if (cd->exec() != QDialog::Accepted) exit(0);
+		QString initref("corbaloc:iiop:");
+		initref += ui.host->text() + ":" + ui.port->text() + "/AuthService";
+		try {
+			CORBA::Object_var obj = orb->string_to_object(initref.toUtf8().constData());
+			VersionControl::Auth_var authref = VersionControl::Auth::_narrow(obj);
+			return authref->login(
+				ui.username->text().toUtf8().constData(),
+				ui.password->text().toUtf8().constData());
+		}
+		catch (VersionControl::AccessDenied& e) {
+			QMessageBox mb(QMessageBox::Critical, "Access Denied",
+				"Check your username and password and try again.",
+				QMessageBox::Ok, mw);
+			mb.exec();
+		}
+		catch (VersionControl::DbError& e) {
+			QMessageBox mb(QMessageBox::Critical, "Database error",
+				"A database error occured on the server, please try again.",
+				QMessageBox::Ok, mw);
+			mb.exec();
+		}
+		catch(CORBA::TRANSIENT&) {
+			QMessageBox mb(QMessageBox::Critical, "Connection error",
+				"Unable to contact the server. Check the host/port and try again.",
+				QMessageBox::Ok, mw);
+			mb.exec();
+		}
+	}
+}
+
+void buildTree(Ui::MainWindow &ui, VersionControl::Root_ptr root) {
+	QTreeWidget *tw = ui.treeWidget;
+	tw->setColumnCount(1);
+	QList<QTreeWidgetItem *> items;
+	VersionControl::ModelSeq *models = root->getModels();
+	for (unsigned int i = 0; i < models->length(); i++) {
+		VersionControl::Model_var model = (*models)[i];
+		QString name = QString::fromUtf8(model->getName());
+		items.append(new QTreeWidgetItem((QTreeWidget*)0, QStringList(name)));
+	}
+	tw->insertTopLevelItems(0, items);
 }
 
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
-	QWidget *mw = showMainWindow();
-	showConnectDialog(mw);
+	const char* options[][2] = {
+		{ "nativeCharCodeSet", "UTF-8" },
+		{ 0, 0 }
+	};
+	CORBA::ORB_var orb = CORBA::ORB_init(argc, argv, "omniORB4", options);
+	QMainWindow *mw = new QMainWindow;
+	Ui::MainWindow ui;
+	ui.setupUi(mw);
+	mw->show();
+	VersionControl::Root_ptr root = showConnectDialog(mw, orb);
+	buildTree(ui, root);
 	return app.exec();
 }
