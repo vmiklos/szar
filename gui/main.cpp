86e50278be7fe31deb0fafb3b5418d235cee1cae
András Veres-Szentkirályi <vsza@vsza.hu>
2010-11-09 11:49:25 +0100

    Added connect dialog

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,12 +1,25 @@
 #include "ui_main.h"
+#include "ui_connect.h"
+
+QWidget *showMainWindow() {
+	QMainWindow *mw = new QMainWindow;
+	Ui::MainWindow ui;
+	ui.setupUi(mw);
+	mw->show();
+	return mw;
+}
+
+void showConnectDialog(QWidget *mw) {
+	QDialog *cd = new QDialog(mw, Qt::WindowSystemMenuHint | Qt::WindowTitleHint);
+	Ui::ConnectDialog ui;
+	ui.setupUi(cd);
+	if (cd->exec() != QDialog::Accepted) exit(0);
+}
 
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
-	QMainWindow *mw = new QMainWindow;
-	Ui::MainWindow ui;
-	ui.setupUi(mw);
-
-	mw->show();
+	QWidget *mw = showMainWindow();
+	showConnectDialog(mw);
 	return app.exec();
 }
