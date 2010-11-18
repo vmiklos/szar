fc3f88a2d78f39ee12bd93f33dcce85799cdc2c0
Miklos Vajna <vmiklos@frugalware.org>
2010-11-18 11:51:52 +0100

    server: store mysql settings in config

--- server/server.cc
+++ server/server.cc
@@ -5,12 +5,51 @@
 using namespace std;
 
 bool db_init() {
+	// Builtin defaults
+	QString hostName("localhost");
+	QString databaseName("swar");
+	QString userName("root");
+	QString password("");
+
+	// Load settings
+	QSettings settings("IRC Software Development", "SQL Version Control");
+	if (settings.value("mysql/host").toString() != "")
+		hostName = settings.value("mysql/host").toString();
+	if (settings.value("mysql/database").toString() != "")
+		databaseName = settings.value("mysql/database").toString();
+	if (settings.value("mysql/user").toString() != "")
+		userName = settings.value("mysql/user").toString();
+
+	// Confirm, so that settings can be modified
+	string ret;
+	cout << "MySQL hostname? [" << hostName.toStdString() << "] ";
+	getline(cin, ret);
+	if (ret.length())
+		hostName = ret.c_str();
+	cout << "MySQL database name? [" << databaseName.toStdString() << "] ";
+	getline(cin, ret);
+	if (ret.length())
+		databaseName = ret.c_str();
+	cout << "MySQL username? [" << userName.toStdString() << "] ";
+	getline(cin, ret);
+	if (ret.length())
+		userName = ret.c_str();
+	cout << "MySQL password? ";
+	getline(cin, ret);
+
 	QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
-	db.setHostName("localhost");
-	db.setDatabaseName("swar");
-	db.setUserName("root");
-	db.setPassword("");
-	return db.open();
+	db.setHostName(hostName);
+	db.setDatabaseName(databaseName);
+	db.setUserName(userName);
+	db.setPassword(ret.c_str());
+	bool success = db.open();
+	if (success) {
+		// Successful login, save settings
+		settings.setValue("mysql/host", hostName);
+		settings.setValue("mysql/database", databaseName);
+		settings.setValue("mysql/user", userName);
+	}
+	return success;
 }
 
 int main(int argc, char **argv)
