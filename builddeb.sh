55886f31178fc45291eea459f3501ac659cdeab2
András Veres-Szentkirályi <vsza@vsza.hu>
2010-11-24 00:10:30 +0100

    debs: add SQL schema to server package

--- builddeb.sh
+++ builddeb.sh
@@ -1,12 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 EXEC=$1
 APP=sql-version-control-$EXEC
 if [ -e tmp ]; then echo "Please remove 'tmp' directory"; exit 1; fi
 make $EXEC || exit 1
-mkdir -p tmp/DEBIAN
-sed s/__IS__/$((`stat -c '%s' $EXEC/$EXEC` / 1024))/ $EXEC/control >tmp/DEBIAN/control
 mkdir -p tmp/usr/bin
 ln $EXEC/$EXEC tmp/usr/bin/$APP
+if [ "$EXEC" == "server" ]; then
+	mkdir -p tmp/usr/share/sql-version-control;
+	ln VersionControl.sql tmp/usr/share/sql-version-control;
+fi
+mkdir -p tmp/DEBIAN
+sed s/__IS__/`du -s tmp | cut -f 1`/ $EXEC/control >tmp/DEBIAN/control
 cd tmp
 md5sum usr/bin/$APP >DEBIAN/md5sums
 cd ..
