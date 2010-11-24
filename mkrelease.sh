31fd48d636541b046bdf0b32c0d93423294a8737
Miklos Vajna <vmiklos@frugalware.org>
2010-11-24 12:00:12 +0100

    make release: add nsis

--- mkrelease.sh
+++ mkrelease.sh
@@ -21,13 +21,16 @@ done
 # build source code
 make
 make debs
-# FIXME make nsis
+# get nsis
+rm -f sql-version-control-setup.exe
+wget http://vsza.hu/sql-version-control-setup.exe
 # install source code
 make srctarball
 cp sql-version-control-1.0.tar.gz sql-version-control-1.0-release/src/
 # install binaries
 cp sql-version-control-{gui,server}-1.0_i386.deb sql-version-control-1.0-release/bin/
-# FIXME install nsis
+# install nsis
+cp sql-version-control-setup.exe sql-version-control-1.0-release/bin/
 # create release zip
 zip -r sql-version-control-1.0-release.zip sql-version-control-1.0-release
 rm -rf sql-version-control-1.0-release
